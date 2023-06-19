8e3513fa12a9ab6cf04070f7dd7067c46a697791
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-06-19 01:43:36 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 # include <stdio.h>
+
 /**
  * main - a program that prints a line with puts function
  *
@@ -6,6 +7,6 @@
  */
 int main(void)
 {
-   puts(*\"Programming is like building a multilingual puzzle");
-return (0);
+    puts(*\"Programming is like building a multilingual puzzle");
+    return (0);
 }	
