2c7680776303eceafd7e0fa776e63ad3bc4fc7fd
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-06-19 00:11:03 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+# include <studio.h>
+/**
+ * main - a program that prints a line with puts function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts(*\"Programming is like building a multilingual puzzle");
+    return (0);
+}	
