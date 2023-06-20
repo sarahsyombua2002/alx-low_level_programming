e9a0052184bd41c87921e6b37757a21ae6c4bb67
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-06-20 07:52:52 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,7 +9,6 @@ int main(void)
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
-	_putchar('p');
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
@@ -18,4 +17,4 @@ int main(void)
 	_putchar('\n');
 	return (0);
 }
-	
+
