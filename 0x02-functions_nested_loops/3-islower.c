5e355bbccc30cfdd636d4dd611a942218cb20d5a
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-06-20 17:15:41 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - lowercase characters
+ * Returns: 1 for lowercase character or 0 for anyhing else
+ */
+
+int _islower(int c)
+
+{
+if (c >= 97 && c <= 122)returb;
+{
+return (1);
+}
+return (0);
+}
