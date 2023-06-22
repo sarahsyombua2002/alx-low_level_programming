171a733eca5cd563578f14c89f142b95f625e702
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-06-22 09:47:15 -0700

    I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * print_most_numbers - writes the numbers 0 to 9
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			putchar(i + '0');
+		}
+		{
+			putchar('\n');
+		}
+	}
+}
