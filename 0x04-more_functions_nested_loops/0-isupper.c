4aed6921d6ef8473d4565f2dd02330dca43cb453
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-06-22 06:16:19 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ * _putchar - checks for upercase character
+ * @c: The character to print
+ *
+ * Return: On success 1 otherwise 0
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _isupper(int c)
+{
+
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	return (0);
+}
