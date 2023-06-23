62d390b2ac3fede1915f1cd02879566a936ab5a2
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-06-23 01:10:33 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - prints number 1 to 100
+ * multiple three prints fizz instead of a number
+ * multiple five prints of buzz instead of a number
+ * Return: On success 0.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		} else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		} else if (i == 1)
+		{
+			printf("%d", i);
+		} else
+		{
+			printf(" %d", i);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
