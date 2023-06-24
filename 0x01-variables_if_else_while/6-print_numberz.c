8386495641d2dcd7e1ee0b8f3e716846ae62b566
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-06-24 08:12:39 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints all single digit numbers
+ *
+ * Return: Always (0) success
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
