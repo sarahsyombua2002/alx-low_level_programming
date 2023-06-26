c5b46d212dd1acf80b09a7cb7e9f4d916e7933b1
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-06-26 11:51:16 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function that updates the value
+ * @n: it takes the pointer to an intial
+ *
+ * Return: Always (0) success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
