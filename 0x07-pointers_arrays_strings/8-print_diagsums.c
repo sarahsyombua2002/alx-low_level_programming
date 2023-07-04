09d676f18527399a000288c6c8a9cd3b792672b7
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-07-04 03:21:33 -0700

    The line of life is a ragged diagonal between duty and desire

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -20,6 +20,5 @@ for  (i = 0; i < size; i++)
 sum1 += *(a + i * size + i);
 sum2 += *(a + i * size + (size - 1 - i));
 }
-printf("%d\n", sum1);
-printf("%d\n", sum2);
+printf("%d, %d\n", sum1, sum2);
 }
