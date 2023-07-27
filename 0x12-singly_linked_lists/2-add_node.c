d74e761f93701927bd5fa9edef55e71869818ff2
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-07-27 06:39:05 -0700

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -31,3 +31,21 @@ list_t *add_node(list_t **head, const char *str)
 
 	return (new_node);
 }
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(const char *s)
+{
+	int c = 0;
+
+	while (*s)
+	{
+		s++;
+		c++;
+	}
+
+	return (c);
+}
