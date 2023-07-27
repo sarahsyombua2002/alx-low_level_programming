e824992c76a99bf5f2afb80676245e3687998922
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-07-27 06:37:07 -0700

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -19,7 +19,13 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->str = (char *) str;
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
 	new_node->next = *head;
 	*head = new_node;
 
