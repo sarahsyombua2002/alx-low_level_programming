270f3295b17418e368ed7c6b274f39112b146f7c
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-07-27 05:12:14 -0700

    changes

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
 #ifndef _LINKED_LIST
-#define _LINKED _LIST
+#define _LINKED_LIST
 
 /**
  * struct list_s - singly linked list
@@ -11,9 +11,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 int _putchar(char c);
@@ -23,4 +23,5 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 int _strlen(const char *s);
-#endif
+
+#endif /* _LINKED_LIST */
