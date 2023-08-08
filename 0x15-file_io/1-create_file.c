22dadd2037bcd9eaddad21e8fdbeaa361696dafc
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-08-07 23:59:52 -0700

    Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file.
+ * @filename:  name of the file to create
+ * @text_content: terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ * if the file already exists, truncate it
+ * if filename is NULL return -1
+ * if text_content is NULL create an empty file
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
